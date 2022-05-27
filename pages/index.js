444b4e929457f083cb59f4dde1221e0f28976424
Julian_Cin <ntanh0794@gmail.com>
2022-05-28 04:56:34 +0700

    feat: setup sanity + tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { client } from "../lib/client";
 
-const index = () => {
-  return <div>index</div>;
+const Home = ({ author }) => {
+  console.log(author);
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 };
 
-export default index;
+export const getServerSideProps = async () => {
+  const contactQuery = `*[_type == "contact"]`;
+  const contact = await client.fetch(contactQuery);
+  const authorQuery = `*[_type == "author"]`;
+  const author = await client.fetch(authorQuery);
+  return {
+    props: {
+      contact,
+      author,
+    },
+  };
+};
+
+export default Home;
