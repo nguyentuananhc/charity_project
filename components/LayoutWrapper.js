dbd51f5b19bc2531b17c004e0af87d033f522b72
Julian <ntanh0794@gmail.com>
2022-05-30 00:07:30 +0700

    feat: add layout wrapper

---
+++ components/LayoutWrapper.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import SectionContainer from './SectionContainer'
+import headerNavLinks from '@/data/headerNavLinks'
+import siteMetadata from '@/data/siteMetadata'
+import Link from './Link'
+import Footer from './Footer'
+import ThemeSwitch from './ThemeSwitch'
+import MobileNav from './MobileNav'
+
+function LayoutWrapper({ children }) {
+  return (
+    <SectionContainer>
+      <div className="flex flex-col justify-between h-screen">
+        <header className="flex items-center justify-between py-10">
+          <div>
+            <Link href="/" aria-label={siteMetadata.headerTitle}>
+              <div className="flex items-center justify-between">
+                <div className="mr-3">{/* <Logo /> */}</div>
+              </div>
+            </Link>
+          </div>
+          <div className="flex items-center text-base leading-5">
+            <div className="hidden sm:block">
+              {headerNavLinks.map((link) => (
+                <Link
+                  key={link.title}
+                  href={link.href}
+                  className="p-1 font-medium text-gray-900 dark:text-gray-100 sm:p-4"
+                >
+                  {link.title}
+                </Link>
+              ))}
+            </div>
+            <ThemeSwitch />
+            <MobileNav />
+          </div>
+        </header>
+        <main className="mb-auto">{children}</main>
+        <Footer />
+      </div>
+    </SectionContainer>
+  )
+}
+
+export default LayoutWrapper
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,22 @@
-import React from "react";
-import { client } from "../lib/client";
+import React from 'react'
+import { client } from '../lib/client'
 
 const Home = ({ author }) => {
-  console.log(author);
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-};
+  console.log(author)
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+}
 
 export const getServerSideProps = async () => {
-  const contactQuery = `*[_type == "contact"]`;
-  const contact = await client.fetch(contactQuery);
-  const authorQuery = `*[_type == "author"]`;
-  const author = await client.fetch(authorQuery);
+  const contactQuery = `*[_type == "contact"]`
+  const contact = await client.fetch(contactQuery)
+  const authorQuery = `*[_type == "author"]`
+  const author = await client.fetch(authorQuery)
   return {
     props: {
       contact,
       author,
     },
-  };
-};
+  }
+}
 
-export default Home;
+export default Home
